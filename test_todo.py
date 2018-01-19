4d44b73d3488ac44f9fe734d785c4bab03a2a8b7
Eric Smith <esmith@8thlight.com>
2018-01-19 08:55:08 -0600

    Compleed todo list
    Well there's no output - I have to do that :).

--- test_todo.py
+++ test_todo.py
@@ -1,5 +1,119 @@
 import unittest
+import todo
+import io
 
 class TestTodo(unittest.TestCase):
-    def test_assert_truth(self):
-        self.assertTrue(True)
+    def setUp(self):
+        self.notepad = todo.Notepad()
+
+    def test_with_a_blank_sheet(self):
+        self.assertEqual(self.notepad.todos(), tuple())
+
+    def test_write_todo(self):
+        todoItem = "This is a todo item"
+        self.notepad.write(todoItem)
+
+        self.assertEqual(self.notepad.todos(), (todoItem,))
+
+    def test_many_todos(self):
+        todoItemOne = "One"
+        todoItemTwo = "Two"
+        self.notepad.write(todoItemOne)
+        self.notepad.write(todoItemTwo)
+
+        self.assertEqual(self.notepad.todos(), (todoItemOne, todoItemTwo))
+
+    def test_remove_todo(self):
+        todoItem = "This is a todo item"
+        self.notepad.write(todoItem)
+        self.notepad.remove(todoItem)
+
+        self.assertEqual(self.notepad.todos(), tuple())
+
+
+class TestFileCabinet(unittest.TestCase):
+    def setUp(self):
+        self.stream = io.StringIO()
+
+    def test_writing_an_empty_list_to_the_file_cabinet(self):
+        cabinet = todo.FileCabinet(self.stream)
+        todoList = todo.Notepad()
+
+        cabinet.store(todoList)
+        self.assertEqual(self.stream.getvalue(), '')
+
+    def test_writing_a_list_with_one_entry_to_the_file_cabinet(self):
+        cabinet = todo.FileCabinet(self.stream)
+        todoList = todo.Notepad()
+        todoList.write("Write a todo list app")
+
+        cabinet.store(todoList)
+        self.assertEqual(self.stream.getvalue(), 'Write a todo list app')
+
+    def test_writing_many_entries_to_the_file_cabinet(self):
+        cabinet = todo.FileCabinet(self.stream)
+        todoList = todo.Notepad()
+        todoList.write("Write a todo list app")
+        todoList.write("Profit")
+
+        cabinet.store(todoList)
+        self.assertEqual(self.stream.getvalue(), 'Write a todo list app\nProfit')
+
+    def test_load_an_existing_todo_from_stream(self):
+        self.stream.write("todo 1\n")
+        self.stream.write("todo 2")
+        self.stream.seek(0)
+        cabinet = todo.FileCabinet(self.stream)
+        todoList = todo.Notepad()
+        cabinet.takeOut(todoList)
+
+        self.assertEqual(todoList.todos(), ("todo 1", "todo 2"))
+
+    def test_append_to_the_next_todos_on_store(self):
+        self.stream.write("todo 1\n")
+        self.stream.seek(0)
+        cabinet = todo.FileCabinet(self.stream)
+        todoList = todo.Notepad()
+        cabinet.takeOut(todoList)
+        todoList.write("todo 2")
+        cabinet.store(todoList)
+
+        self.assertEqual(self.stream.getvalue(), "todo 1\ntodo 2")
+
+class TestConsole(unittest.TestCase):
+
+    class FakeChannel:
+        def __init__(self, inputs):
+            self._inputs = inputs
+
+        def input(self):
+            return self._inputs.pop(0)
+
+        def output(self, text):
+            pass
+
+    def test_ends_when_input_is_no(self):
+        channel = TestConsole.FakeChannel(['n'])
+        stream = io.StringIO()
+        console = todo.Console(channel, stream)
+        console.start()
+
+        self.assertEqual(stream.getvalue(), "")
+
+    def test_accepts_a_todo_after_a_y(self):
+        channel = TestConsole.FakeChannel(['y', 'schedule birthday', 'n'])
+        stream = io.StringIO()
+        console = todo.Console(channel, stream)
+        console.start()
+
+        self.assertEqual(stream.getvalue(), "schedule birthday")
+
+    def test_loads_the_initial_todos_from_the_stream(self):
+        channel = TestConsole.FakeChannel(['y', 'Second Todo','n'])
+        stream = io.StringIO()
+        stream.write("First todo\n")
+        stream.seek(0)
+        console = todo.Console(channel, stream)
+        console.start()
+
+        self.assertEqual(stream.getvalue(), "First todo\nSecond Todo")
---
+++ todo.py
@@ -0,0 +1,57 @@
+class FileCabinet:
+    def __init__(self, stream):
+        self._stream = stream
+
+    def store(self, notepad):
+        self._stream.seek(0)
+        self._stream.writelines('\n'.join(notepad.todos()))
+
+    def takeOut(self, notepad):
+        for line in self._stream.readlines():
+            notepad.write(line.strip())
+
+class Notepad:
+    def __init__(self):
+        self._todos = []
+
+    def todos(self):
+        return tuple(self._todos)
+
+    def write(self, todo):
+        self._todos.append(todo)
+
+    def remove(self, todo):
+        self._todos.remove(todo)
+
+class IOChannel:
+    def __init__(self, i, o):
+        self._input = i
+        self._output = o
+
+    def input(self):
+        return self._input()
+
+    def output(self, text):
+        self._output(text)
+
+class Console:
+    def __init__(self, channel, stream):
+        self._channel = channel
+        self._fileCabinet = FileCabinet(stream)
+        self._todolist = Notepad()
+
+    def start(self):
+        self._fileCabinet.takeOut(self._todolist)
+        while self._channel.input() != 'n':
+            self._todolist.write(self._channel.input())
+
+        self._fileCabinet.store(self._todolist)
+
+if __name__ == "__main__":
+    channel = IOChannel(input, print)
+    try:
+        stream = open("todo.txt", "r+")
+        console = Console(channel, stream)
+        console.start()
+    finally:
+        stream.close()
